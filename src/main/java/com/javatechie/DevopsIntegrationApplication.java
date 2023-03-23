05e57cc964cd22d5c7333107391bffb94034a47d
tchocothepierre <tchocothepierre@gmail.com>
2023-03-23 09:33:12 +0100

    message 1

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to Mr. DevOps Youtube Channel";
+		return "welcome to my CI/CD Project";
 	}
 
 	public static void main(String[] args) {
